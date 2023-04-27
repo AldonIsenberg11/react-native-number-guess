f875bcea3f4b4e20b64ec06133df20fab29f5d2b
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-04-26 20:06:00 -0500

    Linear Gradient - change background

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient 
+      style={styles.rootScreen}
+      colors={['#4e0329', '#ddb52f']}
+    >
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   )
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: '#ddb52f'
   }
 });
