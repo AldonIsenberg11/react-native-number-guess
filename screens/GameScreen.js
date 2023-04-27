cb50040c0fa4e845bd29768eb812744665078897
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-04-26 23:38:51 -0500

    Colors - Manage Globally

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 
+import Title from '../components/Title'
+
 function GameScreen() {
     return (
         <View style={styles.screen}>
-            <Text>Game Screen!</Text>
+            <Title>Opponent's Guess</Title>
             <View>
                 <Text>Higher or lower?</Text>
             </View>
