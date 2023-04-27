56e1ad5a85e28799a3ada9bce8108645da1c87c5
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-04-26 20:13:04 -0500

    Background Image - add/combine with gradient

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <LinearGradient 
-      style={styles.rootScreen}
-      colors={['#4e0329', '#ddb52f']}
-    >
-      <StartGameScreen />
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground 
+        source={require('./assets/images/background.png')} 
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   )
 }
@@ -17,5 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
